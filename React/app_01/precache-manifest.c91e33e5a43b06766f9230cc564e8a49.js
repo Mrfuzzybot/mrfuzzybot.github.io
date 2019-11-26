94fdb770a598f7bd834a19e2767be6fae7ea2361
Mrfuzzybot <zhmish228@yandex.ru>
2019-11-26 21:20:00 +0300

    app deploy

---
+++ React/app_01/precache-manifest.c91e33e5a43b06766f9230cc564e8a49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1ac5f902b2a41fc12037ba0abf48d1",
+    "url": "/React/app_01/index.html"
+  },
+  {
+    "revision": "8b883943a0f360be590b",
+    "url": "/React/app_01/static/css/3.b88d47ee.chunk.css"
+  },
+  {
+    "revision": "b7e646f6890b078dc13c",
+    "url": "/React/app_01/static/css/4.a226b800.chunk.css"
+  },
+  {
+    "revision": "0f58590d9a0ec8331f38",
+    "url": "/React/app_01/static/css/main.9d7aeaa2.chunk.css"
+  },
+  {
+    "revision": "72f9488c23e2785bc212",
+    "url": "/React/app_01/static/js/2.ed7727bb.chunk.js"
+  },
+  {
+    "revision": "8b883943a0f360be590b",
+    "url": "/React/app_01/static/js/3.fa741f81.chunk.js"
+  },
+  {
+    "revision": "b7e646f6890b078dc13c",
+    "url": "/React/app_01/static/js/4.944ec91a.chunk.js"
+  },
+  {
+    "revision": "0f58590d9a0ec8331f38",
+    "url": "/React/app_01/static/js/main.b26cadd2.chunk.js"
+  },
+  {
+    "revision": "4934ec88efaea88ceba7",
+    "url": "/React/app_01/static/js/runtime-main.8ae4bc35.js"
+  },
+  {
+    "revision": "816a4ffbbc8a9cb8a1c547e941d64bc1",
+    "url": "/React/app_01/static/media/user.816a4ffb.png"
+  }
+]);
