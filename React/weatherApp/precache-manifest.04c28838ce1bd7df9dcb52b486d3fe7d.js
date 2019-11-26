0893a70fd5780bc48df290fe5b0773f4fcdecf26
Mrfuzzybot <zhmish228@yandex.ru>
2019-11-26 21:50:37 +0300

    weather app

---
+++ React/weatherApp/precache-manifest.04c28838ce1bd7df9dcb52b486d3fe7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f204465f3b306006fc8068aff43dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b07fd47e445a9a4faac",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e97e5382b40921f376c4",
+    "url": "/static/css/main.fec2a0af.chunk.css"
+  },
+  {
+    "revision": "4b07fd47e445a9a4faac",
+    "url": "/static/js/2.86264d78.chunk.js"
+  },
+  {
+    "revision": "e97e5382b40921f376c4",
+    "url": "/static/js/main.0bde06e4.chunk.js"
+  },
+  {
+    "revision": "c08f4b2aaed6d95fd00f",
+    "url": "/static/js/runtime-main.7b922349.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  }
+]);
