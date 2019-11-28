f3e42f7959b1ef929dc51640841226a69be67bef
Mrfuzzybot <zhmish228@yandex.ru>
2019-11-28 17:31:11 +0300

    added paralax effect

--- Banket/js/main.js
+++ Banket/js/main.js
@@ -1,3 +1,13 @@
 // Include you JavaScript Librarys. The following is an example import
 ////= library/youpathonjsfile.js
 // You JavaScript Code
+
+const parallax = (event) => {
+  const elements = document.querySelectorAll('.element')
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i]
+
+    element.style.transform = `translate(${event.clientX / 100}px,${event.clientY / 100}px )`;
+  }
+}
+document.querySelector('body').addEventListener('mousemove', parallax);
