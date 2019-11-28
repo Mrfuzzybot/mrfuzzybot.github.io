82205023b3a15c47ee396f97c5d16995e6a5891c
Mrfuzzybot <zhmish228@yandex.ru>
2019-11-28 20:32:22 +0300

    style changes started adaptive

--- Banket/js/main.min.js
+++ Banket/js/main.min.js
@@ -2,7 +2,7 @@
 ////= library/youpathonjsfile.js
 // You JavaScript Code
 
-const parallax = (event) => {
+const parallax = event => {
   const elements = document.querySelectorAll('.element')
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i]
@@ -11,3 +11,29 @@ const parallax = (event) => {
   }
 }
 document.querySelector('body').addEventListener('mousemove', parallax);
+
+
+const tabsMenu = (items, content, desc) => {
+  for (let i = 0; i < items.length; i++) {
+    let menuItem = items[i];
+    let contentItem = content[i];
+    let contentDescItem = desc[i];
+
+    menuItem.onclick = function () {
+      for (let j = 0; j < items.length; j++) {
+        items[j].classList.remove('active');
+        content[j].classList.remove('visible');
+        desc[j].classList.remove('visible')
+      }
+      menuItem.classList.add('active');
+      contentItem.classList.add('visible');
+      contentDescItem.classList.add('visible')
+    }
+  }
+}
+
+const iceCreamImage = document.querySelectorAll('.iceCream__image')
+const iceCreamDesc = document.querySelectorAll('.iceCream__desc')
+const iceCreamTab = document.querySelectorAll('.tabs__item-hover')
+
+tabsMenu(iceCreamTab, iceCreamImage, iceCreamDesc)
